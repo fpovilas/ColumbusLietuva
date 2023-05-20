33548781eca38722bcbab8b8427122965e33fd75
Povilas 'Povka' Frismantas <f.povilas@gmail.com>
2023-05-20 15:04:33 +0300

    Parašyta pirma užduotis

--- Program.cs
+++ Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool arPasirinko = false;
+            do
+            {
+                SpausdintiMenu();
+                string pasirinkimas = Console.ReadLine();
+                switch (pasirinkimas)
+                {
+                    case "1":
+                        Uzduotis1();
+                        arPasirinko = true;
+                        break;
+                    case "2":
+                        //Uzduotis2();
+                        arPasirinko = true;
+                        break;
+                    case "3":
+                        //Uzduotis3();
+                        arPasirinko = true;
+                        break;
+                    case "4":
+                        //Uzduotis4();
+                        arPasirinko = true;
+                        break;
+                    default:
+                        Console.WriteLine("Blogas pasirinkimas");
+                        arPasirinko = false;
+                        break;
+                }
+            } while(!arPasirinko);
+        }
+
+        private static void Uzduotis1()
+        {
+            Console.WriteLine("Įveskite žodį: ");
+            string zodis = Console.ReadLine();
+
+            for(int i = zodis.Length - 1; i >= 0; i--) { Console.Write(zodis[i]); }
+        }
+
+        private static void SpausdintiMenu()
+        {
+            Console.WriteLine("1.");
+            Console.WriteLine("2.");
+            Console.WriteLine("3.");
+            Console.WriteLine("4.");
+            Console.Write("Jūsų pasirinkimas: ");
         }
     }
 }
